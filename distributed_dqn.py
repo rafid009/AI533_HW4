e6af183a787e4d7b870d0ef7d0ec3604e2682da5
u91532 <u91532@s001-n058.aidevcloud>
2021-12-05 16:43:35 -0800

    dist DQN converges. still takes too long

--- distributed_dqn.py
+++ distributed_dqn.py
@@ -1,7 +1,9 @@
+from torch.optim.adam import Adam
 from memory_remote import ReplayBuffer_remote
 from dqn_model import _DQNModel
 import torch
 from custom_cartpole import CartPoleEnv
+
 import torch
 import time
 import os
@@ -11,12 +13,23 @@ import numpy as np
 from tqdm import tqdm
 from random import uniform, randint
 
-from dqn_model import DQNModel
 
 import matplotlib.pyplot as plt
 from filelock import FileLock
 
+import torch.nn as nn
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# Set the Env name and action space for CartPole
+ENV_NAME = 'CartPole_distributed'
+
+# Set result saveing floder
+result_floder = ENV_NAME + "_distributed"
+result_file = ENV_NAME + "/results.txt"
+if not os.path.isdir(result_floder):
+    os.mkdir(result_floder)
+torch.set_num_threads(12)
 
 FloatTensor = torch.FloatTensor
 
@@ -41,27 +54,24 @@ hyperparams_CartPole = {
     'use_target_model': True
 }
 
-ENV_NAME = 'CartPole_distributed'
-
-# Set result saveing floder
-result_floder = ENV_NAME
-result_file = ENV_NAME + "/results.txt"
-if not os.path.isdir(result_floder):
-    os.mkdir(result_floder)
-torch.set_num_threads(12)
-
+start = time.time()
 
 @ray.remote
 class Model_Server(object):
-    def __init__(self, hyper_params, input_len, memory, training_episodes, test_intervals, action_space=len(ACTION_DICT)):
+    def __init__(self, hyper_params, input_len, memory, training_episodes, test_interval, action_space=len(ACTION_DICT)):
         super().__init__()
         self.action_space = action_space
         output_len = action_space
 
-        self.eval_model = DQNModel(input_len, output_len, learning_rate = hyper_params['learning_rate'])
+        self.eval_model = nn.DataParallel(_DQNModel(input_len, output_len))
+        self.optimizer = Adam(self.eval_model.parameters(), lr = hyper_params['learning_rate'])
+        self.loss_fn = nn.SmoothL1Loss()
+
         self.use_target_model = hyper_params['use_target_model']
         if self.use_target_model:
-            self.target_model = DQNModel(input_len, output_len)
+            self.target_model = nn.DataParallel(_DQNModel(input_len, output_len))
+
+        self.prev_eval_models = []
 
         self.batch_size = hyper_params['batch_size']
         self.update_steps = hyper_params['update_steps']
@@ -73,28 +83,73 @@ class Model_Server(object):
         self.epsilon_decay_steps = hyper_params['epsilon_decay_steps']
         
         self.episodes = 0
+        self.prev_episode = 0
+
         self.steps = 0
+        self.prev_update = 0
+        self.prev_replace = 0
+        
+        self.updates_made = 0
         
         # memory server
         self.memory = memory
 
         self.training_episodes = training_episodes
-        self.test_intervals = test_intervals
-        self.total_test_num = self.training_episodes // self.test_intervals
+        self.test_interval = test_interval
+        self.total_test_num = self.training_episodes // self.test_interval
 
         self.best_reward = 0
         self.results = [0] * (self.total_test_num + 1)
         self.result_counts = 0
         self.eval_ends = False
 
+    # model stuffs
+    def predict(self, input, model):
+        input = FloatTensor(input).unsqueeze(0)
+        q_values = model(input)
+        action = torch.argmax(q_values)
+
+        return action.item()
+    
+    def predict_batch(self, input, model):
+        input = FloatTensor(input)
+        q_values = model(input)
+        values, q_actions = q_values.max(1)
+        
+        return q_actions, q_values
+
+    def fit(self, q_values, target_q_values, optimizer, loss_fn):
+        loss = loss_fn(q_values, target_q_values)  
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        self.updates_made += 1
+        
+    def replace(self):
+        self.target_model.load_state_dict(self.eval_model.state_dict())
+    
+    def save(self, path):
+        torch.save(self.eval_model.state_dict(), path + '/best_model.pt')
+        
+    def load(self, path):
+        self.eval_model.load_state_dict(torch.load(path + '/best_model.pt'))
+
+
+    ## agent stuffs
     def increase_episodes(self):
         self.episodes += 1
+
+        if self.episodes // self.test_interval + 1 > len(self.prev_eval_models): 
+            print('collector episode: ', self.episodes, ' time: ', time.time() - start)
+            self.prev_eval_models.append(self.eval_model)
+            
         if self.episodes >= self.training_episodes:
             return True
         return False
 
     def increase_steps(self):
         self.steps += 1
+        return self.steps
 
     def get_best_reward(self):
         return self.best_reward
@@ -121,12 +176,15 @@ class Model_Server(object):
             return self.greedy_policy(state)
         
     def greedy_policy(self, state):
-        return self.eval_model.predict(state)
+        return self.predict(state, self.eval_model)
 
     def update(self):
-        if self.steps % self.update_steps == 0 and ray.get(self.memory.get_length.remote()) >= self.batch_size:
+        self.steps += 1
+        if self.steps % self.update_steps == 0 and  self.steps > self.prev_update and \
+        ray.get(self.memory.get_length.remote()) >= self.batch_size:
+            self.prev_update = self.steps
             batch = ray.get(self.memory.sample.remote(self.batch_size))
-            print('update ', self.steps)
+            # print('update ', self.steps)
             (states, actions, reward, next_states,
             is_terminal) = batch
             
@@ -138,23 +196,24 @@ class Model_Server(object):
                                     dtype=torch.long)
             
             # Current Q Values
-            _, q_values = self.eval_model.predict_batch(states)
+            _, q_values = self.predict_batch(states, self.eval_model)
             q_values = q_values[batch_index, actions]
             
             # Calculate target
             if self.use_target_model:
-                actions, q_next = self.target_model.predict_batch(next_states)
+                actions, q_next = self.predict_batch(next_states, self.target_model)
             else:
-                actions, q_next = self.eval_model.predict_batch(next_states)
+                actions, q_next = self.predict_batch(next_states, self.eval_model)
                 
             #INSERT YOUR CODE HERE --- neet to compute 'q_targets' used below
             q_target = (terminal * reward) + (1.0 - terminal) * (reward + (self.beta * torch.max(q_next, 1)[0]))
 
             # update model
-            self.eval_model.fit(q_values, q_target)
+            self.fit(q_values, q_target, self.optimizer, self.loss_fn)
 
-        if self.use_target_model and self.steps % self.model_replace_freq == 0:
-            self.target_model.replace(self.eval_model)
+        if self.use_target_model and self.steps % self.model_replace_freq == 0 and self.steps > self.prev_replace:
+            self.prev_replace = self.steps
+            self.replace()
 
     def add_result(self, result, num):
         self.results[num] = result
@@ -163,52 +222,45 @@ class Model_Server(object):
         return self.results
 
     def ask_evaluation(self):
-        if self.episodes != 0 and self.episodes % self.test_intervals == 0:
-            print('episode ', self.episodes)
-            print('ask')
+        if len(self.prev_eval_models) > self.result_counts:
             num = self.result_counts
+            eval_model = self.prev_eval_models[num]
             self.result_counts += 1
-            return False, num
+        #if self.episodes != 0 and self.episodes % self.test_intervals == 0 and self.episodes > self.prev_episode:
+            print('episode ', self.episodes, ' time: ', time.time()-start)
+            print('ask')
+            return eval_model, False, num
         else:
-            if self.result_counts >= self.total_test_num:
+            if self.episodes >= self.training_episodes:
                 self.eval_ends = True
-            return self.eval_ends, None
-
-    # save model
-    def save_model(self):
-        self.eval_model.save(result_floder + '/best_model.pt')
-        
-    # load model
-    def load_model(self):
-        self.eval_model.load(result_floder + '/best_model.pt')
+            return None, self.eval_ends, None
 
     def write_result_to_file(self, result_file, avg_result):
         # Create a filelock object. Consider using an absolute path for the lock.
         if avg_result >= self.best_reward:
             self.best_reward= avg_result
-            self.save_model()
+            self.save(result_floder)
         with FileLock(result_file):
             with open(result_file, "a+") as f:
                 f.write(str(avg_result) + "\n")
                 f.close()
 
-
-
 @ray.remote
 class Collector(object):
-    def __init__(self, model_server, env, memory):
+    def __init__(self, model_server, env, memory, update_steps):
         super().__init__()
         self.model_server = model_server
         self.env = env
         self.max_episode_steps = env._max_episode_steps
         self.memory = memory
+        self.update_steps = update_steps
 
     def generate_experiences(self):
         while True:
             state = self.env.reset()
             done = False
             steps = 0
-            print('collector')
+            # print('collector')
             while steps < self.max_episode_steps and not done:
                 #INSERT YOUR CODE HERE
                 # add experience from explore-exploit policy to memory
@@ -216,12 +268,12 @@ class Collector(object):
                 # update the target network (if the target network is being used) every 'model_replace_freq' of experiences 
                 action = ray.get(self.model_server.explore_or_exploit_policy.remote(state))
                 steps += 1
-                ray.get(self.model_server.increase_steps.remote())
 
                 next_state, reward, done, info = self.env.step(action)
                 
                 self.memory.add.remote(state, action, reward, next_state, done)
                 state = next_state
+                self.model_server.update.remote()
 
                 # ray.get(self.model_server.update.remote())
             do_continue = ray.get(self.model_server.increase_episodes.remote())
@@ -238,12 +290,15 @@ class Evaluator(object):
         self.max_episode_steps = env._max_episode_steps
         self.model_server = model_server
 
+    def greedy_policy(self, state, model):
+        return self.model_server.predict.remote(state, model)
+
     def evaluate(self):
         while True:
-            eval_ends, num = ray.get(self.model_server.ask_evaluation.remote())
+            eval_model, eval_ends, num = ray.get(self.model_server.ask_evaluation.remote())
             if eval_ends:
                 break
-            if num is None:
+            if eval_model is None:
                 continue
             print('eval')
             total_reward = 0
@@ -260,15 +315,15 @@ class Evaluator(object):
 
             avg_reward = total_reward / self.trials
             self.model_server.add_result.remote(avg_reward, num)
-
-            self.model_server.write_result_to_file.remote(result_file, avg_reward)
+            print('reward: ', avg_reward)
+            # self.model_server.write_result_to_file.remote(result_file, avg_reward)
             
     
-    def save_model(self):
-        self.eval_model.save(result_floder + '/best_model.pt')
+    # def save_model(self):
+    #     self.eval_model.save(result_floder + '/best_model.pt')
 
-    def load_model(self):
-        self.eval_model.load(result_floder + '/best_model.pt')
+    # def load_model(self):
+    #     self.eval_model.load(result_floder + '/best_model.pt')
     
 def plot_result(total_rewards ,learning_num, legend):
     print("\nLearning Performance:\n")
@@ -294,12 +349,13 @@ class distributed_DQN_agent(object):
         self.cw_num = cw_num
         self.ew_num = ew_num
         self.eval_trials = eval_trials
+        self.update_steps = hyper_params['update_steps']
 
     def learn_and_evaluate(self):
         workers_id = []
         for cw in range(self.cw_num):
             simulator = CartPoleEnv()
-            collector = Collector.remote(self.model_server, simulator, self.memory)
+            collector = Collector.remote(self.model_server, simulator, self.memory, self.update_steps)
             workers_id.append(collector.generate_experiences.remote())
         for ew in range(self.ew_num):
             simulator = CartPoleEnv()
@@ -310,18 +366,15 @@ class distributed_DQN_agent(object):
         
 
 cw_num = 4
-ew_num = 2
+ew_num = 4
 training_episodes = 10000
 test_intervals = 50
 eval_trials = 30
 action_space = len(ACTION_DICT)
 simulator = CartPoleEnv()
 input_len = len(simulator.reset())
-start = time.time()
 dist_DQN = distributed_DQN_agent(cw_num, ew_num, input_len, training_episodes, test_intervals, eval_trials, hyperparams_CartPole, action_space)
 result = dist_DQN.learn_and_evaluate()
 end = time.time()
 print('time: ', end-start, ' s')
 plot_result(result, test_intervals, ["batch_update with target_model"])
-
-
